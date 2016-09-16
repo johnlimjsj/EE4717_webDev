74db09ec052646b4d2280398aca9901bb7b0408d
johnlimjsj <johnlimjsj@gmail.com>
2016-09-16 11:56:25 +0800

    added add to cart feature with pop up

--- project/static/js/generateContent/inDivTabs.js
+++ project/static/js/generateContent/inDivTabs.js
@@ -13,6 +13,7 @@
 	function selectTab(){
 		var tab=$("ul.tab>li");
 		tab.addClass("tablinks");
+		$("div.firsttab").show();
 		tab.click(function(e){
 			let tabName = $(this).attr('tab');
 			let tabcontent = $(".tabcontent");
@@ -39,7 +40,7 @@
 			let full_imgsrc = '<img src="../../static/img/shop/'+ thissrc +'.png">';
 			let display_title = $('<div>'+thistitle+'</div>').addClass('item-title');
 			let display_price = $('<div>'+'$'+thisprice+'</div>').addClass('item-price');
-			let addtocart_button = $('<button>Add to Cart</button>').attr('data-price',thisprice);
+			let addtocart_button = $('<button>Add to Cart</button>').attr('data-price',thisprice).attr('data-title',thistitle);
 			let addtocart_container = $('<div></div>').addClass('center-div').append(addtocart_button);
 			$(this).append(full_imgsrc).append(display_title).append(display_price).append(addtocart_container);
 		});
@@ -47,8 +48,22 @@
 		
 		$('button').click(function(){
 			var thisprice = $(this).data('price');
-			var totalprice = totalprice+ thisprice;
-			alert(thisprice);
+			var thistitle= $(this).data('title');
+			let modal = $('.modal');
+			let modaltext = $('p.modal-text');
+			let closebtn = $('span.close');
+			// document.getElementById("john").innerHTML = "bfeffewef";
+			let message = $("<p>"+ thistitle+" costing $" + thisprice + " was added to your shopping cart </p>");
+			message.addClass('modal-text');
+
+			modaltext.replaceWith(message);
+			modal.show();
+			closebtn.click(function(){
+				modal.hide();
+			});
+
+
+
 		});
 
 	}
