0c5be33ff1ad2c3cba89d23543fa63430ca71a6b
johnlimjsj <johnlimjsj@gmail.com>
2016-11-03 14:31:50 +0800

    process search

--- project/templates/shop/processsearch.php
+++ project/templates/shop/processsearch.php
@@ -11,10 +11,12 @@ include 'functions.php';
   // generateSearchArray('name', 'Colours');
   // generateSearchArray('name', 'Style');
   $flag=1;
-  $select = "SELECT * FROM Products WHRE ";
+  $select = "SELECT * FROM Products WHERE ";
   $select = generateSearchStringFromParamTableKeyArray($select, 'cat_id', 'name', 'Categories');
   $select = generateSearchStringFromParamTableKeyArray($select, 'colour_id', 'name', 'Colours');
   $select = generateSearchStringFromParamTableKeyArray($select, 'style_id', 'name', 'Style');
+
+  // wildcard search
   echo $select;
 
   $result=$db->query($select);
